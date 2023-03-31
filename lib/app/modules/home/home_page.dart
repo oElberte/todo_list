23cba32322131c03ddfabadf32a6bf56896ad755
Elberte Plinio <elbertedoolan@gmail.com>
2023-03-31 19:19:33 -0300

    add home page layout

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_provider/app/core/auth/auth_provider.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_tasks.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_week_filter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,10 +14,55 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        iconTheme: IconThemeData(color: context.primaryColor),
+        backgroundColor: const Color(0xFFFAFBFE),
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) {
+              return [
+                const PopupMenuItem<bool>(
+                  child: Text('Mostrar tarefas concluídas'),
+                ),
+              ];
+            },
+            icon: const Icon(TodoListIcons.filter),
+          )
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: context.primaryColor,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: const Color(0xFFFAFBFE),
       drawer: HomeDrawer(),
-      body: Container(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      HomeHeader(),
+                      HomeFilters(),
+                      HomeWeekFilter(),
+                      HomeTasks(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/app/modules/home/widgets/task.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatelessWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: IntrinsicHeight(
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(8),
+          leading: Checkbox(
+            value: true,
+            onChanged: (value) {},
+          ),
+          title: const Text(
+            'Descrição',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          subtitle: const Text(
+            '31/03/2023',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(width: 1),
+          ),
+        ),
+      ),
+    );
+  }
+}
