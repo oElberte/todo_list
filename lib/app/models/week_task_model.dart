7cebcba1526ad37ffd3f18029224f5ed72b5ace9
Elberte Plinio <elbertedoolan@gmail.com>
2023-04-04 09:54:41 -0300

    add repository and service for get tasks

---
+++ lib/app/models/week_task_model.dart
@@ -0,0 +1,14 @@
+
+import 'package:todo_list_provider/app/models/task_model.dart';
+
+class WeekTaskModel {
+  final DateTime startDate;
+  final DateTime endDate;
+  final List<TaskModel> tasks;
+  
+  const WeekTaskModel({
+    required this.startDate,
+    required this.endDate,
+    required this.tasks,
+  });
+}
